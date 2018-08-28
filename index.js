a3677ba1d4226e5efb36c09dca61b8d10df3fb44
Joel Edwards <joeledwards@gmail.com>
2018-08-28 10:26:31 -0600

    feat: updated all dependencies and corrected code as needed

--- index.js
+++ index.js
@@ -2,7 +2,7 @@
 
 const
 	bole    = require('bole'),
-	Squeaky = require('squeaky'),
+	squeaky = require('squeaky'),
 	url     = require('url')
 	;
 
@@ -18,7 +18,7 @@ class NSQRelayer
 	constructor({ nsq = 'nsq://localhost:4150', topic = 'relayed', event = 'nsq' })
 	{
 		const parsed = url.parse(nsq);
-		this.nsq = new Squeaky({ host: parsed.hostname, port: parsed.port || 4150 });
+		this.nsq = new squeaky.Publisher({ host: parsed.hostname, port: parsed.port || 4150 });
 		this.topic = topic;
 		this.eventName = event;
 		this.logger = bole(event);
@@ -41,7 +41,7 @@ class NSQRelayer
 
 	close()
 	{
-		this.nsq.close()
+		this.nsq.close();
 		process.removeListener(this.eventName, this.handler);
 	}
 }
